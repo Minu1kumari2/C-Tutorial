8b4e5f9b52091d95c640c85f5a5b36c08cd44e51
Minu1kumari2 <123921637+Minu1kumari2@users.noreply.github.com>
2025-09-18 11:15:16 +0000

    c# tutorial

---
+++ StringApp/Program.cs
@@ -0,0 +1,102 @@
+﻿// See https://aka.ms/new-console-template for more information
+/*
+Console.WriteLine("Hello, World!");
+string name = Console.ReadLine();
+string candies = Console.ReadLine();
+Console.WriteLine($"Your name is {name}. You will get {candies} candies");
+string hello = "Hello World this\" is Minu";
+Console.WriteLine(hello.IndexOf("is"));
+Console.WriteLine(hello.Substring(5));
+ */
+
+/*
+Console.WriteLine("Enter your age");
+
+string agestr = Console.ReadLine();
+int age = Convert.ToInt32(agestr);
+bool isBanned = true;
+if (age < 2 && isBanned)
+{
+    Console.WriteLine("You are just born");
+}
+else if (age < 18)
+{
+    Console.WriteLine("You are below 18");
+}
+else if (age < 75)
+{
+    Console.WriteLine("You can drive");
+}
+else
+{
+    Console.WriteLine("You can not drive");
+}
+
+Console.ReadLine();
+*/
+/*
+int age = 18;
+switch (age)
+{
+    case 18:
+        Console.WriteLine("Please wait for an year");
+        break;
+    case 20:
+        Console.WriteLine("you are 20");
+        break;
+    default:
+        Console.WriteLine("Enjoy!");
+        break;
+}
+*/
+//Loops
+/*
+int i = 0;
+while (i < 5)
+{
+    Console.WriteLine(i);
+    i++;
+}
+*/
+/*
+int i = 0;
+do
+{
+    Console.WriteLine(i + 1);
+    i++;
+} while (i < 5);
+*/
+
+/*
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine(i);
+}
+*/
+//Break and continue
+/*
+for (int i = 0; i < 6; i++)
+{
+    if (i == 3)
+    {
+        continue;
+    }
+    Console.WriteLine(i);
+    if (i == 3)
+    {
+        break;
+    }
+
+*/
+//method
+/*
+static void Greet(string name)
+{
+    Console.WriteLine("Good Morning " + name);
+}
+
+Greet("Minu");
+*/
+//oops - classes and objects
+
+
